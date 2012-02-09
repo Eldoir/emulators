01641bef723e22aaa311274aabadc35893bf72f6
Mike <michael@emidee.net>
2012-02-09 15:31:02 +0100

    Added the Processor class, with its registers

--- GameboyEmulator/GameboyEmulator/GameboyEmulator/Emulator.cs
+++ GameboyEmulator/GameboyEmulator/GameboyEmulator/Emulator.cs
@@ -7,6 +7,8 @@ namespace GameboyEmulator
 {
     public class Emulator
     {
+        private Processor processor = new Processor();
+        
         public void Load( byte[] rom )
         {
             byte[] gameNameBytes = new byte[16];
@@ -14,6 +16,10 @@ namespace GameboyEmulator
             Array.Copy(rom, 0x134, gameNameBytes, 0, 16);
 
             GameName = System.Text.Encoding.ASCII.GetString( gameNameBytes );
+
+            processor.Registers.AF = 12345;
+
+            Console.WriteLine(processor.Registers.AF);
         }
 
         public string GameName { get; private set; }
